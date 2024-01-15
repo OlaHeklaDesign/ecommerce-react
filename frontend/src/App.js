b3b6af7eaba9bd979f5c50f6a9f7aff93c7780aa
OlaHeklaDesign <155102472+OlaHeklaDesign@users.noreply.github.com>
2024-01-15 19:43:26 +0100

    Structure of Hero Section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
+import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ShopCategory from './Pages/ShopCategory';
 import Shop from "./Pages/Shop";
--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Hero from "../Components/Hero/Hero.jsx"
 
 const Shop = () => {
   return (
     <div>
-
+      <Hero/>
     </div>
   )
 }
