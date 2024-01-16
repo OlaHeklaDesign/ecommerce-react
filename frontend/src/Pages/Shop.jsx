ace29c8963097a71ab36fc7adde4f0ce563d87a3
OlaHeklaDesign <155102472+OlaHeklaDesign@users.noreply.github.com>
2024-01-16 14:48:22 +0100

    Items, Popular, Offers Components added

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import Hero from "../Components/Hero/Hero.jsx"
+import Hero from "../Components/Hero/Hero.jsx";
+import Popular from "../Components/Popular/Popular.jsx";
+import Offers from "../Components/Offers/Offers.jsx";
 
 const Shop = () => {
   return (
     <div>
       <Hero/>
+      <Popular/>
+      <Offers/>
     </div>
   )
 }
